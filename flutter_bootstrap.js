2acf3d995fb8af1e482df5c9ffd26f58cd3cd7e0
bekuluk7 <bekuluk7@gmail.com>
2026-08-21 00:21:15 +0600

    Собрано из verbalingo 4ada6c9

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "43955589" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1548721819" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
